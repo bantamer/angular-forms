b012ce5fbbab2cdf24eeb197cc52c0981643a1d0
Piotr Pazhytnykh <pazhitnykhpetr@gmail.com>
2025-05-07 11:47:42 +0200

    remove behavior subject

--- src/users/users-table/users-table.component.ts
+++ src/users/users-table/users-table.component.ts
@@ -1,22 +1,58 @@
-import { AsyncPipe, DatePipe } from '@angular/common';
-import { Component, input, output } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
-import { User } from 'users/users-service/user';
+import { DatePipe } from '@angular/common';
+import { Component, computed, input, output, viewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { User, UserField } from 'users/users-service/user';
 import { MatIcon } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 import { UsersTableColumn } from './users-table.types';
-import { Observable } from 'rxjs';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { getSortType, SortType } from './users-table-sort/get-sort-type';
+import { sortDate } from './users-table-sort/sort-data';
+import { sortDefault } from './users-table-sort/sort-default';
+import { Order } from './users-table-sort/types';
+
+const usersGridComparator = (data: User[], sort: MatSort) => {
+  const isAsc = sort.direction === Order.Asc;
+  const key = sort.active as UserField;
+
+  if (!key || !data.length) {
+    return data;
+  }
+
+  const sortType = getSortType<UserField, User[]>(data, key);
+
+  return data.sort((a, b) => {
+    switch (sortType) {
+      case SortType.Date:
+        return sortDate(a[key] as Date, b[key] as Date, isAsc);
+      default:
+        return sortDefault(a[key] as string, b[key] as string, isAsc);
+    }
+  });
+};
 
 @Component({
   selector: 'app-user-table',
   standalone: true,
-  imports: [MatTableModule, MatIcon, MatButtonModule, DatePipe, AsyncPipe],
+  imports: [MatTableModule, MatIcon, MatButtonModule, MatSortModule, DatePipe],
   templateUrl: './users-table.component.html',
 })
 export class UsersTableComponent {
-  readonly users$ = input.required<Observable<User[]>>();
+  readonly users = input.required<User[]>();
   readonly deleteUserEvent = output<string>();
+
+  readonly sort = viewChild(MatSort);
+
+  public dataSource = computed(() => {
+    const sort = this.sort();
+    const source = new MatTableDataSource<User>(this.users());
+
+    source.sort = sort ?? null;
+    source.sortData = usersGridComparator;
+
+    return source;
+  });
 
   displayedColumns: string[] = [
     UsersTableColumn.Actions,
--- src/users/users.component.ts
+++ src/users/users.component.ts
@@ -3,43 +3,64 @@ import { Component } from '@angular/core';
 import { UsersFormComponent } from './users-form/users-form.component';
 import { UsersTableComponent } from './users-table/users-table.component';
 import { User } from './users-service';
-import { BehaviorSubject, interval, map, startWith, switchMap } from 'rxjs';
+import {
+  interval,
+  map,
+  merge,
+  scan,
+  startWith,
+  Subject,
+  switchMap,
+  takeWhile,
+} from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UsersFormComponent, UsersTableComponent],
+  imports: [UsersFormComponent, UsersTableComponent, AsyncPipe],
   template: `
     <app-user-form (submitEvent)="onUserSubmit($event)" />
     <app-user-table
-      [users$]="users$"
+      [users]="(users$ | async) ?? []"
       (deleteUserEvent)="onUserDelete($event)"
     />
   `,
 })
 export class HomeComponent {
-  private usersSubject = new BehaviorSubject<User[]>([]);
-  public users$ = this.usersSubject.asObservable().pipe(
+  private userAdded$ = new Subject<User>();
+  private userDeleted$ = new Subject<string>();
+
+  public users$ = merge(
+    this.userAdded$.asObservable(),
+    this.userDeleted$.asObservable(),
+  ).pipe(
+    scan((acc, event) => {
+      if (typeof event === 'string') {
+        return acc.filter((user) => user.uuid !== event);
+      }
+      return [...acc, event];
+    }, [] as User[]),
+
     switchMap((users) =>
       interval(1000).pipe(
-        startWith(users),
+        startWith(0),
         map(() => {
           return users.map(({ accountBalance, ...user }) => ({
             ...user,
             accountBalance: accountBalance + Math.floor(Math.random() * 10000),
           }));
         }),
+        takeWhile((users) => users.length > 0, true),
       ),
     ),
   );
 
   onUserSubmit(user: User) {
-    this.usersSubject.next([...this.usersSubject.value, user]);
+    this.userAdded$.next(user);
   }
 
   onUserDelete(uuid: string) {
-    this.usersSubject.next(
-      this.usersSubject.value.filter((user) => user.uuid !== uuid),
-    );
+    this.userDeleted$.next(uuid);
   }
 }
